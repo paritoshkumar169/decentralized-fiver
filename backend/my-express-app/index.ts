2ed8d9130c06c8c7fc23c2b15435de9055c122f3
paritoshkumar169 <74917758+paritoshkumar169@users.noreply.github.com>
2024-05-18 01:38:13 +0530

    Update index.ts

--- backend/my-express-app/index.ts
+++ backend/my-express-app/index.ts
@@ -1,4 +1,4 @@
 import express from "express";
 const app = express();
 
-// postgresql://idk_owner:le4Hu9jhBrdA@ep-twilight-hill-a1oovdh0.ap-southeast-1.aws.neon.tech/idk?sslmode=require
+// postgresql://idk_owner:.ap-southeast-1.aws.neon.tech/idk?sslmode=require
